b9e2ecb491a6558e2064ed236a2dd19a23cebadb
maariagarrcia <mrrmn@myuax.com>
2022-03-24 09:12:26 +0100

    comentarios

--- CrearCuentas.py
+++ CrearCuentas.py
@@ -7,7 +7,19 @@ import random
 from colorama import *
 
 ### C L A S E
+# Creacion de una cuenta que refleje una cuenta bancaria
+# Para poder crear la plantilla CreadorCuenta hay que tener en cuenta
+# las ACCIONES QUE POODEMOS REALIZAR CON ELLA :
+#           - Tranferir dinero
+#           - Ingresar dinero
+#           - Retirar dinero, entre otras....
+# En algunas cuentas bancarias hay que retirar "x" dinero antes de un dia fijado,
+# es decir, antes de la fecha de vencimiento (EXPLICADO  EN SU RESPECTIVA CLASE),QUE SEAN ALEATORIAS LAS FECHAS.
+# Además a esta cuenta le añadiremos  que tenga  una atributo adicional: EL POSIBLE
+# DINERO EN NEGATIVO ---> . En las cuentas Vip uno podrá tener saldo negativo siempre que no supere este valor.
 
+### Por lo tanto vemso que es NECESARIO EL USO DE LA HERENCIA ya qu emuchas veces hay que 
+# reutilizar codigoo y hacer unos pequeñps cambios
 class CreadorCuenta():
     # Recibe como argumento un objeto y retorna otro objeto que sirve como
     # identificador único para el primero. Devuelve un ENTERO
